f0d78575a68a816bc4bac78f0b1b7c56a63e7541
arnavhu9-aa <bhanarkararnav9@gmail.com>
2026-05-28 15:25:28 +0530

    Update api.js

--- aichat/src/api.js
+++ aichat/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://chat-production-c642.up.railway.app";
+export const API_BASE_URL = "https://chat-production-1ed5.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
